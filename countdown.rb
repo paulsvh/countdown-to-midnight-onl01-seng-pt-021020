63c4b2ba65684302de299edd04590514debf5384
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-12 03:15:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,6 +6,12 @@ def countdown (number)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(number)
+  while number >= 1
+    puts "#{number} SECOND(S)!"
+    sleep(2)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
 
 end
